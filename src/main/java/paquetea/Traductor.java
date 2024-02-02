971a33a187062228b86a494a3314dadbfdf6864c
None083 <perezgonzaleznoelia08@gmail.com>
2024-02-02 10:06:35 +0100

    wfd

---
+++ src/main/java/paquetea/Traductor.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package paquetea;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author noelia
+ */
+public class Traductor {
+
+    private SortedMap<String, String> traduccion;
+
+    public Traductor() {
+        this.traduccion = new TreeMap<>();
+    }
+    
+    
+    
+}
